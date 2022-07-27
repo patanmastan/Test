ea83655c8b00f18b3328cc5fafe34673b9d7c8bf
Mastan <patanmastan018@gmial.com>
2022-07-27 23:34:20 +0530

    comment added

--- src/main/java/com/example/demo/StudentDemoApplication.java
+++ src/main/java/com/example/demo/StudentDemoApplication.java
@@ -11,7 +11,7 @@ public class StudentDemoApplication {
 		SpringApplication.run(StudentDemoApplication.class, args);
 	}
 	
-	
+	// comment
 	@GetMapping("/msg")
 	public String msg() {
 		return "Hi ! everyone";
